cbb0a970c1fe1f2d100823b70cc3e816f425e32f
Don Mutua <print.idd@gmail.com>
2020-09-06 23:56:23 +0300

    increase logo size

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         >
           Powered by{" "}
           <img
-            style={{ width: "100px" }}
+            style={{ height: "2em" }}
             src="/socialdukalogo.svg"
             alt="social duka Logo"
             className={styles.logo}
